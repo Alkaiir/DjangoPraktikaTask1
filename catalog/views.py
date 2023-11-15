8b87d0a7cbd0376dd50f8fc8a53ea78b29fd8300
Alkaiir <ivv.ven0@gmail.com>
2023-11-15 13:07:54 +0700

    Сделано задание 5

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from catalog.models import BookInstance, Author, Book, Genre
+
+
+def index(request):
+    # num_books_with_a = Book.objects.filter(title__icontains='a').count()
+    # num_genres_with_a = Genre.objects.filter(name__icontains='a').count()
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        # 'num_books_with_a': num_books_with_a,
+        # 'num_genres_with_a': num_genres_with_a
+    }
+
+    return render(request, 'index.html', context=context)
